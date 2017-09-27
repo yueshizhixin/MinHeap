be7aca8917f535bbaa6f5bf3c2372c406a1771e9
Robert Adam Sedgwick <rasedgwick@gmail.com>
2017-09-27 12:28:25 -0400

    Enqueu() working

--- MinHeap/MinHeap.cs
+++ MinHeap/MinHeap.cs
@@ -5,9 +5,38 @@ using System.Text;
 
 namespace MinHeap
 {
-    class MinHeap<T> : IEnumerable<T>, IReadOnlyCollection<T>
+    public class AnonymousComparer<T> : IComparer<T>
+    {
+        private Comparison<T> comparison;
+
+        public AnonymousComparer(Comparison<T> comparison)
+        {
+            if (comparison == null)
+                throw new ArgumentNullException("comparison");
+            this.comparison = comparison;
+        }
+
+        public int Compare(T x, T y)
+        {
+            return comparison(x, y);
+        }
+    }
+
+    public class MinHeap<T> 
     {
         public int Count { get; set; }
+        public T[] Heap { get; set; }
+        private readonly int defaultSize = 100;
+        //private Comparer<T> Comparer;
+        AnonymousComparer<T> Comparer;
+
+        public MinHeap(Comparison<T> comparison)
+        {
+            Heap = new T[defaultSize];
+            Comparer = new AnonymousComparer<T>(comparison);
+            Count = 0;
+
+        }
 
         public int Clear()
         {
@@ -19,9 +48,37 @@ namespace MinHeap
             throw new NotImplementedException();
         }
 
-        public int Enqueue(T item)
+        public void Enqueue(T item)
         {
-            throw new NotImplementedException();
+
+            if (Count + 1 > Heap.Length - 1)
+                Resize();
+
+            // Insert to end of array
+            Count++;
+            Heap[Count] = item;
+            int index = Count;
+            
+
+            while (index > 0)
+            {
+                int parentIndex = index / 2;
+                T parent = Heap[parentIndex];
+                
+                if (Comparer.Compare(item, parent) < 0)
+                {
+                    // Swap
+                    Heap[parentIndex] = item;
+                    Heap[index] = parent;
+                    index = parentIndex;
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+            
         }
 
         public int Dequeue(T item)
@@ -44,20 +101,12 @@ namespace MinHeap
             throw new NotImplementedException();
         }
 
-
-        public void CopyTo(Array array, int index)
+        private bool Resize()
         {
-            throw new NotImplementedException();
+
+            return true;
         }
 
-        public IEnumerator<T> GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
 
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
     }
 }
---
+++ MinHeapTests/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MinHeap;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MinHeapTests
+{
+
+    [TestClass]
+    public class UnitTest1
+    {
+            
+
+        [TestMethod]
+        public void Enqueue()
+        {
+            // Arrange
+            MinHeap<int> heap = new MinHeap<int>((x, y) => x.CompareTo(y));
+            // Act
+            heap.Enqueue(1);
+            heap.Enqueue(10);
+
+
+            // Assert
+            Assert.IsTrue(heap.Heap[0] < heap.Heap[1]);
+        }
+    }
+}
