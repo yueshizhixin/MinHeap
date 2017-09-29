5e3f88c0f686f3ad20f62cc68beb04e4fc366847
Robert Adam Sedgwick <rasedgwick@gmail.com>
2017-09-29 09:23:27 -0400

    Basic functionality implemented

--- MinHeap/MinHeap.cs
+++ MinHeap/MinHeap.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Text;
 
 namespace MinHeap
 {
-
     public class MinHeap<T> where T : IComparable
     {
         public List<T> _data;
@@ -18,30 +15,89 @@ namespace MinHeap
         public void Enqueue(T item)
         {
             _data.Add(item);
-
-            int child = _data.Count - 1;                        // child index; start at end
+            int child = _data.Count - 1;     
+            
             while (child > 0)
             {
-                int parent = (child - 1) / 2;                       // parent index
+                int parent = (child - 1) / 2;                    
 
                 if (_data[child].CompareTo(_data[parent]) >= 0) 
-                    break;                                         // child item is larger than (or equal) parent so we're done
+                    break;                                          // child item is larger than (or equal) parent so we're done
 
                 Swap(ref _data, ref child, ref parent);
-                //T temp = _data[child];
-                //_data[child] = _data[parent];
-                //_data[parent] = temp;
-                //child = parent;
             }
         }
 
-
-        private void Swap(ref List<T> data, ref int child, ref int parent)
+        public T Dequeue()
         {
-            T temp = data[child];
-            data[child] = data[parent];
-            data[parent] = temp;
-            child = parent;
+            if (_data.Count == 0)
+                throw new InvalidOperationException("Heap Empty");
+
+            int last = _data.Count - 1;
+            T first = _data[0];
+            _data[0] = _data[last];
+            _data.RemoveAt(last);
+            int parent = 0;
+
+            last--;
+
+            while (true)
+            {
+                int left = parent * 2 + 1;               // Left child index of parent
+
+                if (left > last)
+                    break;
+
+                int right = left + 1;                    // Right child index
+
+                // If there is a right child, and it is smaller then the left child....
+                if (right <= last && _data[right].CompareTo(_data[left]) < 0)
+                    left = right;
+
+                // If parent is <= to samllest child; done.
+                if (_data[parent].CompareTo(_data[left]) <= 0)
+                    break;
+
+                Swap(ref _data, ref parent, ref left);
+            }
+
+            return first;
+        }
+
+        public T Peek()
+        {
+            if (_data.Count == 0)
+                throw new InvalidOperationException("Heap Empty");
+
+            return _data[0];
+        }
+
+        public void Clear()
+        {
+            _data.Clear();
+        }
+
+        public T[] ToArray()
+        {
+            return _data.ToArray();
+        }
+
+        public List<T> ToList()
+        {
+            return _data;
+        }
+
+        public int Count()
+        {
+            return _data.Count;
+        }
+
+        private void Swap(ref List<T> data, ref int x, ref int y)
+        {
+            T temp = data[x];
+            data[x] = data[y];
+            data[y] = temp;
+            x = y;
         }
 
 
--- MinHeapTests/UnitTests.cs
+++ MinHeapTests/UnitTests.cs
@@ -21,10 +21,43 @@ namespace MinHeapTests
             heap.Enqueue(5);
             heap.Enqueue(-10);
 
-
             Assert.AreEqual(heap._data[0], -10);
         }
 
+        [TestMethod]
+        public void Dequeue()
+        {
+            MinHeap<int> heap = new MinHeap<int>();
+            int targetItem = 1;
+            int targetSize = 2;
+
+            heap.Enqueue(10);
+            heap.Enqueue(5);
+            heap.Enqueue(targetItem);
+
+
+            int actualItem = heap.Dequeue();
+            int actualSize = heap.Count();
+
+            Assert.AreEqual(actualItem, targetItem);
+            Assert.AreEqual(actualSize, targetSize);
+        }
+
+        [TestMethod]
+        public void Peek()
+        {
+            MinHeap<int> heap = new MinHeap<int>();
+            int targetItem = -10; 
+
+            heap.Enqueue(10);
+            heap.Enqueue(5);
+            heap.Enqueue(targetItem);
+
+            int actualItem = heap.Peek();
+
+            Assert.AreEqual(actualItem, targetItem);
+
+        }
        
     }
 }
